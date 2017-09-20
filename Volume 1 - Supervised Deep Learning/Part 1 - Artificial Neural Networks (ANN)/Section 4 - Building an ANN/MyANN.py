bc8fafa32063e1f24c0dee25569419d077bd92a2
Aakash Kishore Chotrani <aakashchotrani@gmail.com>
2017-09-20 13:22:40 -0700

    Traing ANN and builing confusion matrix to test the predicted results

--- Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/MyANN.py
+++ Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/MyANN.py
@@ -42,16 +42,33 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense
 
-#Initializing ANN
+#Initializing ANN as sequence of layers
 classifier = Sequential()
 
 #Adding Input Layer and First hidden layer
 #Step 1: Randomly initialize the weights to small numbers close to 0 but not 0
-classifier.add(Dense(output_dim = 6,init = 'uniform',activation = 'relu',input_dim = 11))
+#classifier.add(Dense(output_dim = 6,init = 'uniform',activation = 'relu',input_dim = 11))
+#first hidden layer with 6 output and 11 inputs. Activation is rectified linear and initialized weight with uniform distribution
+classifier.add(Dense(6,activation = 'relu',kernel_initializer = 'uniform',input_shape=(11,)))
+
+#Adding the second hidden layer
+classifier.add(Dense(6,activation = 'relu',kernel_initializer = 'uniform'))
+
+#Adding the output layer, changing the output dimension and the activation to sigmoid
+classifier.add(Dense(1,activation = 'sigmoid',kernel_initializer = 'uniform'))
+
+#compiling the ANN, choosing stochastic gradient descent algorithm = 'adam', it works on the loss function and how to evaluate the performance of the model.
+classifier.compile(optimizer = 'adam',loss = 'binary_crossentropy',metrics= ['accuracy'])
+
+#Fitting Ann to the training set results
+classifier.fit(X_train,y_train,batch_size = 10, epochs = 100)
+
+
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
+y_pred = (y_pred>0.5)
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)f
+cm = confusion_matrix(y_test, y_pred)
